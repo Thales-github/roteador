e6a989601a4a3888d640c6d85c0f969afcd3251b
Thales-github <thalesvitoroliveiracirino@gmail.com>
2022-11-25 23:13:28 -0300

    Atualizações do roteador

--- router.php
+++ router.php
@@ -1,13 +1,15 @@
 <?php
 
-require_once("Exibir.php");
+$pasta = $_SERVER["DOCUMENT_ROOT"] . "/boletimDeNoticias/";
+require_once($pasta . "php/model/Banco_class.php");
 
-$caminho = str_replace("/router/index.php/", "", $_SERVER["REQUEST_URI"]);
+$caminho = $_SERVER["REQUEST_URI"];
 
 $vetor = explode("/", $caminho);
 $classe = $vetor[0];
 $vetor2 = explode("?", $vetor[1]);
-$metodo = $vetor2[0];
-$instancia = new $classe();
+$metodo = $vetor[2];
 
-$instancia->$metodo($_REQUEST);
+$instancia = new Banco();
+
+var_dump($instancia->$metodo());
